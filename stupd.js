06ca1134ce0158b891a8a89496eefab76fd31898
wad218 <wad.2015.98@gmail.com>
2026-02-13 18:26:40 +0200

    Update stupd.js

--- stupd.js
+++ stupd.js
@@ -91,9 +91,9 @@
     }
 
     var style = '<style id="studio-logos-combined-style">' +
-        '.rate--studio.studio-logo { align-items: center; vertical-align: middle; padding: 5px 12px !important; background: rgba(255,255,255,0.1) !important; border-radius: 10px; transition: all 0.2s ease; height: 32px; cursor: pointer; border: 1px solid transparent; }' +
+        '.rate--studio.studio-logo { align-items: center; vertical-align: middle; padding: 5px 12px !important; background: rgba(255,255,255,0.1) !important; border-radius: 10px; transition: all 0.2s ease; height: 30px; cursor: pointer; border: 1px solid transparent; }' +
         '.rate--studio.studio-logo.focus { background: rgba(255,255,255,0.2) !important; border: 1px solid #fff; transform: scale(1.05); }' +
-        '.rate--studio.studio-logo img { max-height: 28px; !important; max-width: 120px; object-fit: contain; }' +
+        '.rate--studio.studio-logo img { max-height: 25px; !important; max-width: 120px; object-fit: contain; }' +
         '.studio-logo-text { font-size: 0.75em; font-weight: bold; color: #fff !important; }' +
     '</style>';
 
